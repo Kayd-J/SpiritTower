7bbe75d3b31d7c3914b36a712ea68a36b673caf7
Jarod <jarodavid@estudiantec.cr>
2020-06-24 22:08:54 -0600

    Simp(le) Character Movement
    Basic Settings of the character and jump detection

---
+++ Unity/SpiritTower/Assets/Scripts/Movement.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] float JumpVel;
+    public Rigidbody rigidbody;
+    float axisX;
+    float axisY;
+    Vector3 axis;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        axisX = Input.GetAxis("Horizontal");
+        axisY = Input.GetAxis("Vertical");
+
+        axis = new Vector3(axisX,0,axisY);
+
+        if (Input.GetButtonDown("Jump")) {
+            rigidbody.velocity = new Vector3(axisX, JumpVel, axisY);
+        }
+        
+    }
+
+    void FixedUpdate() {
+        rigidbody.MovePosition(rigidbody.position + axis);
+    }
+}
