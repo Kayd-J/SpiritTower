9cea97f1a8d4550b3873619bdc17e943657e7531
hazielgr <mariohaziel@gmail.com>
2020-07-14 01:11:52 -0600

    MatrixCode

---
+++ Prueba/Prueba/Algorithms/PathFinding.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "../LinkedList/LinkedList.h"
+#include <iostream>
+#include <list>
+#include"../Matrix/Matrix.h"
+
+using namespace std;
+class PathFinding{
+public:
+	LinkedList searchPath(Matrix &mat,Square* start, Square* end);
+	int heuristic(Square* start, Square* end);
+	bool includes(list<Square*> list, Square* square);
+};
+
