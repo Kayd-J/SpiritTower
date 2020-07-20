12c0c5040af1cf46236cb0600ea2e238461676aa
hazielgr <mariohaziel@gmail.com>
2020-07-19 21:44:13 -0600

    Update

--- Prueba/Prueba/server/messagesManager/JsonHandler.cpp
+++ Prueba/Prueba/server/messagesManager/JsonHandler.cpp
@@ -1,62 +1,53 @@
 #include "JsonHandler.h"
 
-bool JsonHandler::handlingReceivedData(std::string DataOne, Json::Value DataTwo, Json::Value DataThree) {
+bool JsonHandler::handlingReceivedData(Json::Value DataOne, Json::Value DataTwo) {
 
     std::cout << "INFO RECEIVED " << std::endl;
+    int health = DataOne["Health"].asInt();
+    int score = DataOne["Score"].asInt();
+    bool shield = DataOne["Shield"].asBool();
+    bool sword = DataOne["Sword"].asBool();
+    int posX = DataOne["posX"].asInt();
+    int posY = DataOne["posY"].asInt();
 
-    std::cout << "This is the MAP-> " << DataOne << std::endl;
-
-    int health = DataTwo["Health"].asInt();
-    int score = DataTwo["Score"].asInt();
-    bool shield = DataTwo["Shield"].asBool();
-    bool sword = DataTwo["Sword"].asBool();
-
-    std::cout << "This is the PLAYER STATUS -> " << DataTwo << std::endl;
-
-    std::cout << "Can be used One by One :) "<< std::endl;
+    std::cout << "This is the PLAYER STATUS -> " << std::endl;
+    std::cout << "Can be used One by One :) " << std::endl;
     std::cout << "This is the Health-> " << health << std::endl;
     std::cout << "This is the Score-> " << score << std::endl;
     std::cout << "This is the Shield-> " << shield << std::endl;
     std::cout << "This is the Sword-> " << sword << std::endl;
+    std::cout << "This his PosX-> " << posX << std::endl;
+    std::cout << "This his PosY-> " << posY << std::endl;
 
     std::cout << "These are the ENEMIES " << std::endl;
-    std::cout << DataThree << std::endl;
-    auto enemyA = DataThree["A"];
-    auto enemyB = DataThree["B"];
-
-    std::cout << "For Enemie A, his data " << std::endl;
-    bool alert = enemyA["Alert"].asBool();
-    int range = enemyA["Range"].asInt();
-    std::cout << "Already saw the player?-> " << alert << std::endl;
-    std::cout << "This is the Range-> " << range << std::endl;
-
-    std::cout << "For Enemie B, his data " << std::endl;
-    std::cout << enemyB << std::endl;
+    for (int i = 0; i < DataTwo.size(); i++) {
+        std::cout << DataTwo[i] << std::endl;
+        for (int j = 0; j < DataTwo[i].getMemberNames().size(); j++) {
+            std::cout << DataTwo[i].getMemberNames()[j] << ": " << DataTwo[i][DataTwo[i].getMemberNames()[j]].asString() << std::endl;
+        }
+    }
     return true;
 }
 bool JsonHandler::Deserialize(std::basic_string<char> streamOfJson) {
     Json::Value root;
     std::string errors;
-
-    Json::CharReaderBuilder builder;    
+    Json::CharReaderBuilder builder;
     Json::CharReader* reader = builder.newCharReader();
-
-    std::cout << streamOfJson << std::endl;
 
     bool parsingSuccessful = reader->parse(streamOfJson.c_str(), streamOfJson.c_str() + streamOfJson.size(), &root, &errors);
     delete reader;
-
     if (!parsingSuccessful) {
         std::cout << "DataNoParceable" << std::endl;
         return false;
-    }else{
+    }
+    else {
         for (Json::Value::const_iterator i = root.begin(); i != root.end(); i++) {
-            std::string DataOne = root["Matrix"].asString();
-            auto DataTwo = root["Player"];
-            auto DataThree = root["Enemies"];
-            if (handlingReceivedData(DataOne,DataTwo,DataThree)) {
+            auto DataOne = root["Player"];
+            auto DataTwo = root["Enemies"];
+            if (handlingReceivedData(DataOne, DataTwo)) {
                 return true;
-            }else{
+            }
+            else {
                 std::cout << "Something were wrong while handling the Data from Client" << std::endl;
                 return false;
             }
--- Prueba/Prueba/server/messagesManager/JsonHandler.h
+++ Prueba/Prueba/server/messagesManager/JsonHandler.h
@@ -3,10 +3,22 @@
 #include <json/json.h>
 #include <json/value.h>
 #include "../Server.h"
-
-class JsonHandler : public Server{
+/**
+ *  @file JsonHandler.h
+ *  @version 1.0
+ *  @date 17/07/2020
+ *  @author Joseph Jimenez.
+ *  @class JsonHandler.
+ *  @brief Controla los mensajes Json que ingresan al servidor.
+ */
+class JsonHandler : public Server {
 public:
-    static bool handlingReceivedData(std::string DataOne, Json::Value DataTwo, Json::Value DataThree);
+    /**
+      * @brief usa los json obtenidos del mensaje.
+    */
+    static bool handlingReceivedData(Json::Value DataOne, Json::Value DataTwo);
+    /**
+      * @brief obtiene json de cada mensaje entrante.
+    */
     static bool Deserialize(std::basic_string<char> streamOfJson);
 };
-
