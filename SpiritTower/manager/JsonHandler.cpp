6bc6dc4ac9638763e08a857e3b7c3c9658b51f89
Joseph Jimenez <josephdjz@hotmail.com>
2020-07-24 19:27:52 -0600

    Updated Deserialize

--- SpiritTower/manager/JsonHandler.cpp
+++ SpiritTower/manager/JsonHandler.cpp
@@ -1,6 +1,6 @@
-    #include "JsonHandler.h"
+#include "JsonHandler.h"
 
-bool JsonHandler::handlingReceivedData(Json::Value DataOne, Json::Value DataTwo) {
+bool JsonHandler::handlingReceivedData(Json::Value DataOne, Json::Value DataTwo, Json::Value DataThree, Json::Value DataFour, Player*& player) {
 
     std::cout << "INFO RECEIVED " << std::endl;
     int health = DataOne["Health"].asInt();
@@ -10,18 +10,66 @@ bool JsonHandler::handlingReceivedData(Json::Value DataOne, Json::Value DataTwo)
     int posX = DataOne["posX"].asInt();
     int posY = DataOne["posY"].asInt();
 
-    std::cout << "This is the PLAYER STATUS -> " << std::endl;
-    std::cout << "Can be used One by One :) " << std::endl;
-    std::cout << "This is the Health-> " << health << std::endl;
-    std::cout << "This is the Score-> " << score << std::endl;
-    std::cout << "This is the Shield-> " << shield << std::endl;
-    std::cout << "This is the Sword-> " << sword << std::endl;
-    std::cout << "This his PosX-> " << posX << std::endl;
-    std::cout << "This his PosY-> " << posY << std::endl;
+    //------------------------------Enemies-------------------------------------------------------------
+
+    for (int i = 0; i < DataTwo.size(); ++i) {
+        std::cout << DataTwo[i]["COLOR"] << std::endl;
+        std::string colorEnemie = DataTwo[i]["COLOR"].asString();
+        bool deathEnemie = DataTwo[i]["DEATH"].asBool();
+        std::string idEnemie = DataTwo[i]["ID"].asString();
+        int posXEnemie = DataTwo[i]["posX"].asInt();
+        int posYEnemie = DataTwo[i]["posY"].asInt();
+
+        std::cout << "This is the Enemie Color-> " << colorEnemie << std::endl;
+        std::cout << "This is the Enemie Score Death-> " << deathEnemie << std::endl;
+        std::cout << "This is the Enemie ID-> " << idEnemie << std::endl;
+        std::cout << "This his Enemie PosX-> " << posXEnemie << std::endl;
+        std::cout << "This his Enemie PosY-> " << posYEnemie << std::endl;
+    }
+
+    //------------------------------Objects-------------------------------------------------------------
+    for (int i = 0; i < DataThree.size(); ++i) {
+        std::cout << DataThree[i]["COLOR"] << std::endl;
+        bool deathObject = DataThree[i]["DEATH"].asBool();
+        std::string idObject = DataThree[i]["ID"].asString();
+        int posXObject = DataThree[i]["posX"].asInt();
+        int posYObject = DataThree[i]["posY"].asInt();
+
+        std::cout << "This is the Object Score Death-> " << deathObject << std::endl;
+        std::cout << "This is the Object ID-> " << idObject << std::endl;
+        std::cout << "This his Object PosX-> " << posXObject << std::endl;
+        std::cout << "This his Object PosY-> " << posYObject << std::endl;
+    }
+    //------------------------------Boss-------------------------------------------------------------
+    int healthBoss = DataFour["Health"].asInt();
+    int scoreBoss = DataFour["Score"].asInt();
+    bool shieldBoss = DataFour["Shield"].asBool();
+    bool swordBoss = DataFour["Sword"].asBool();
+    int posXBoss = DataFour["posX"].asInt();
+    int posYBoss = DataFour["posY"].asInt();
+
+    std::cout << "This is the BOSS STATUS -> " << std::endl;
+
+    std::cout << "This is the Boss Health-> " << healthBoss << std::endl;
+    std::cout << "This is the Boss Score-> " << scoreBoss << std::endl;
+    std::cout << "This is the Boss Shield-> " << shieldBoss << std::endl;
+    std::cout << "This is the Boss Sword-> " << swordBoss << std::endl;
+    std::cout << "This his Boss PosX-> " << posXBoss << std::endl;
+    std::cout << "This his Boss PosY-> " << posYBoss << std::endl;
+
+
+    system("pause");
+
+    player->posX = posX;
+    player->posY = posY;
+    player->health = health;
+    player->score = score;
+    player->shield = shield;
+    player->sword = sword;
 
     return true;
 }
-bool JsonHandler::Deserialize(std::basic_string<char> streamOfJson) {
+bool JsonHandler::Deserialize(std::basic_string<char> streamOfJson, Player*& player) {
     Json::Value root;
     std::string errors;
     Json::CharReaderBuilder builder;
@@ -37,7 +85,9 @@ bool JsonHandler::Deserialize(std::basic_string<char> streamOfJson) {
         for (Json::Value::const_iterator i = root.begin(); i != root.end(); i++) {
             auto DataOne = root["Player"];
             auto DataTwo = root["Enemies"];
-            if (handlingReceivedData(DataOne, DataTwo)) {
+            auto DataThree = root["Objects"];
+            auto DataFour = root["Boss"];
+            if (handlingReceivedData(DataOne, DataTwo, DataThree, DataFour)) {
                 return true;
             }
             else {
